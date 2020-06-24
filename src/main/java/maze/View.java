0c2ce6b196a12017648897c0062771af8a1100da
Igor <GI_Something@protonmail.com>
2020-06-24 21:11:43 +0300

    feat: Make output (maze) more readable

--- src/main/java/maze/View.java
+++ src/main/java/maze/View.java
@@ -1,7 +1,22 @@
 package maze;
 
+import java.util.List;
+
 public class View {
     public static void main(String[] args) {
-        MazeGenerator.generateMaze(5, 15).forEach(System.out::println);
+        formatAndPrintMaze(MazeGenerator.generateMaze(5, 15));
+    }
+
+    public static void formatAndPrintMaze(List<StringBuilder> maze) {
+        for (StringBuilder row : maze) {
+            for (char elem : row.toString().toCharArray()) {
+                if (elem == ' ') {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("##");
+                }
+            }
+            System.out.println();
+        }
     }
 }
