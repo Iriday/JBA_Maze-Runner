acba89060c271b2df9bfa671adba105c14a3b792
Igor <GI_Something@protonmail.com>
2020-06-26 09:42:35 +0300

    feat: Add main menu
    Menu options:
    1. Generate a new maze
    2. Load a maze          (not implemented)
    3. Save the maze        (not implemented)
    4. Display the maze
    0. Exit

--- src/main/java/maze/View.java
+++ src/main/java/maze/View.java
@@ -1,29 +1,91 @@
 package maze;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class View {
-    public static void main(String[] args) {
-        int[] size = getMazeSizeFromConsole();
-        formatAndPrintMaze(MazeGenerator.generateMaze(size[0], size[1]));
+    private List<StringBuilder> lastMaze;
+
+    public void run() {
+        var scn = new Scanner(System.in);
+        initialMainMenu(scn);
+        mainMenu(scn);
     }
 
-    public static void formatAndPrintMaze(List<StringBuilder> maze) {
-        for (StringBuilder row : maze) {
-            for (char elem : row.toString().toCharArray()) {
-                if (elem == ' ') {
-                    System.out.print("  ");
-                } else {
-                    System.out.print("##"); // or change to - "\u2588\u2588"
+    private void initialMainMenu(Scanner scn) {
+        while (true) {
+            System.out.println("=== Menu ===\n"
+                    + "1. Generate a new maze\n"
+                    + "2. Load a maze\n"
+                    + "0. Exit");
+
+            switch (scn.next().trim()) {
+                case "1" -> {
+                    lastMaze = generateNewMaze(scn);
+                    printMaze(lastMaze);
+                    System.out.println();
+                    return;
                 }
+                case "2" -> System.out.println("Not implemented yet, coming soon.");
+                case "0" -> System.exit(0);
+                default -> System.out.println("Incorrect option, please try again.");
             }
             System.out.println();
         }
     }
 
-    public static int[] getMazeSizeFromConsole() {
-        var scn = new Scanner(System.in);
+    private void mainMenu(Scanner scn) {
+        while (true) {
+            System.out.println("== Menu ==\n"
+                    + "1. Generate a new maze\n"
+                    + "2. Load a maze\n"
+                    + "3. Save the maze\n"
+                    + "4. Display the maze\n"
+                    + "0. Exit");
+
+            switch (scn.next().trim()) {
+                case "1" -> {
+                    lastMaze = generateNewMaze(scn);
+                    printMaze(lastMaze);
+                }
+                case "2", "3" -> System.out.println("Not implemented yet, coming soon.");
+                case "4" -> printMaze(lastMaze);
+                case "0" -> {
+                    return;
+                }
+                default -> System.out.println("Incorrect option, please try again.");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<StringBuilder> generateNewMaze(Scanner scn) {
+        int[] size = getMazeSizeFromConsole(scn);
+        return formatMaze(MazeGenerator.generateMaze(size[0], size[1]));
+    }
+
+    public static List<StringBuilder> formatMaze(List<StringBuilder> maze) {
+        List<StringBuilder> formattedMaze = new ArrayList<>(maze.size());
+        for (StringBuilder row : maze) {
+            var sbRow = new StringBuilder();
+            for (char elem : row.toString().toCharArray()) {
+                if (elem == ' ') {
+                    sbRow.append("  ");
+                } else {
+                    sbRow.append("##"); // or change to - "\u2588\u2588"
+                }
+            }
+            formattedMaze.add(sbRow);
+        }
+        return formattedMaze;
+    }
+
+    public static void printMaze(List<StringBuilder> maze) {
+        maze.forEach(System.out::println);
+    }
+
+    public static int[] getMazeSizeFromConsole(Scanner scn) {
         System.out.println("Please, enter the size of a maze:");
         while (true) {
             int rows = scn.nextInt();
